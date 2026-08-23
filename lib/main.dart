e7dcf4f576d8cbb2e36714ba2c205db5d2a783c2
tarunbuda5-tech <tarunbuda5@gmail.com>
2026-08-23 10:14:04 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -92,7 +92,7 @@ class _HomePageState extends State<HomePage> {
 
   // GitHub repository ka songs.json
   final String songsUrl =
-      'https://cdn.jsdelivr.net/gh/tarunbuda5-tech/KOSLI-DHUN@main/data/songs.json';
+      'https://raw.githubusercontent.com/tarunbuda5-tech/KOSLI-DHUN/main/data/songs.json';
 
   @override
   void initState() {
